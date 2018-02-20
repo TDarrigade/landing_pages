2cdc1dc6f4fee7ca30f3727ee25994818fc3d95d
Thomas 2 <thomas2@MacBook-Pro-de-Thomas.local>
2018-02-20 14:38:57 +0100

    push avec index

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
   describe "GET #checkr" do
     it "returns http success" do
       get :checkr
