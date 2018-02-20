b9a4a16bd1d8b76ff9d3e1bf47161960d31e5481
Thomas 2 <thomas2@MacBook-Pro-de-Thomas.local>
2018-02-20 14:13:34 +0100

    first commit

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+
+  describe "GET #checkr" do
+    it "returns http success" do
+      get :checkr
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #flynn" do
+    it "returns http success" do
+      get :flynn
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
